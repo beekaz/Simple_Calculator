eaeb38b693bd907e92c0623c85c6bc7c9a2bcbc7
Beekaz <sooperfect00@gmail.com>
2023-07-09 18:25:52 +0100

    main_function

--- SIMPLE_CALCULATOR/main_function.py
+++ SIMPLE_CALCULATOR/main_function.py
@@ -1,5 +1,5 @@
 from tkinter import *
 #from arithemetic_functions import add, subtract, multiply, divide
-from calculator_ui_design import *
+from for_multiple_gui_input import create_gui
 #create_gui(root, add,  multiply, divide, subtract)
 frame.mainloop()
