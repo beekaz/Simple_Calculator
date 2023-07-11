427d52e54932a555d1360949018a756d66b97123
Gbenga Adeyi <adeyigbenga005@gmail.com>
2023-07-11 17:28:49 +0100

    Commit local progress

--- simple_calculator/main.py
+++ simple_calculator/main.py
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
 from gui import root
 
 # Run the calculator
+=======
+from calculator_gui import root
+#create_gui(root, add,  multiply, divide, subtract)
+>>>>>>> 4bf7723 (Commit local progress)
 root.mainloop()
